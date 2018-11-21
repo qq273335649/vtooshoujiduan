080763f659c8f0a24aa92cfe76c039d52c4e13a2
wusong <273335649@qq.com>
2018-11-21 10:13:33 +0800

    1.0

---
+++ js/login.js
@@ -0,0 +1,36 @@
+//  import { userLogin } from '../../api/api';
+ 
+  export default {
+    data() {
+      return {
+        userName: '',
+        password: '',
+        isBtnLoading: false
+      }
+    },
+    created () {
+      if(JSON.parse( localStorage.getItem('user')) && JSON.parse( localStorage.getItem('user')).userName){
+        this.userName = JSON.parse( localStorage.getItem('user')).userName;
+        this.password = JSON.parse( localStorage.getItem('user')).password;
+      }
+    },
+    computed: {
+      btnText() {
+        if (this.isBtnLoading) return '登录中...';
+        return '登录';
+      }
+    },
+    methods: {
+      login() {
+        if (!this.userName) {
+          this.$message.error('请输入用户名');
+          return;
+        }
+        if (!this.password) {
+          this.$message.error('请输入密码');
+          return;
+        }
+ 
+      }
+    }
+  }
